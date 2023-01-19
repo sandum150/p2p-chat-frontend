d99da564e4abf35b9dd2c4af101e35caa2352ddc
Alexandru Moldovanu <sandum150@gmail.com>
2023-01-19 12:57:35 +0200

    routing wildcard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
-import {AppComponent} from "./app.component";
 import {RoomComponent} from "./room/room.component";
 
 const routes: Routes = [
     { path: 'room/:sessionId', component: RoomComponent },
-    { path: 'room', component: RoomComponent }
+    { path: 'room', component: RoomComponent },
+    { path: '', redirectTo: 'room' , pathMatch: 'full'},
+    { path: '**', redirectTo: 'room' },
 ];
 
 @NgModule({
